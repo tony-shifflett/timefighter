fd751dfd9e0968b84f6af45a7529114e7c9ffa00
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-02-16 12:11:29 -0600

    added private variables and empty functions to eventually  hold game logic

--- app/src/main/java/com/tonyshifflett/timefighter/MainActivity.kt
+++ app/src/main/java/com/tonyshifflett/timefighter/MainActivity.kt
@@ -2,10 +2,27 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+     
+    private fun incrementScore(){
+        //increment score logic
+    }
+
+    private fun resetGame(){
+        //reset game logic
+    }
+
+    private fun endGame(){
+        //end game logic
     }
 }
