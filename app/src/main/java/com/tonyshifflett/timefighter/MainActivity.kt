c0b9417685065df935bddc8b7b5baf4fc9d57432
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-02-16 12:20:33 -0600

    created score variable, added logic to incrementScore, fixed typo with setOnClickListener

--- app/src/main/java/com/tonyshifflett/timefighter/MainActivity.kt
+++ app/src/main/java/com/tonyshifflett/timefighter/MainActivity.kt
@@ -9,17 +9,22 @@ import android.widget.TextView
     private lateinit var gameScoreTextView: TextView
     private lateinit var timeLeftTextView: TextView
     private lateinit var tapMeButton: Button
+    private var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         gameScoreTextView = findViewById(R.id.game_score_text_view)
         timeLeftTextView = findViewById(R.id.time_left_text_view)
         tapMeButton = findViewById(R.id.tap_me_button)
-        tapMeButton.setOnClickListener(incrementScore())
+        tapMeButton.setOnClickListener { incrementScore() }
     }
 
     private fun incrementScore(){
         //increment score logic
+        score++
+        val newScore = "Your Score: $score"
+        gameScoreTextView.text = newScore
     }
 
     private fun resetGame(){
