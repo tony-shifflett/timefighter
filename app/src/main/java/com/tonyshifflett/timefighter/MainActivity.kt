59f796f1df8aebf336792231851a7520b52c30af
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-02-27 16:15:50 -0600

    modified colors/themes, added bounce animation to TapMe button

--- app/src/main/java/com/tonyshifflett/timefighter/MainActivity.kt
+++ app/src/main/java/com/tonyshifflett/timefighter/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -33,7 +34,10 @@ import android.widget.Toast
         gameScoreTextView = findViewById(R.id.game_score_text_view)
         timeLeftTextView = findViewById(R.id.time_left_text_view)
         tapMeButton = findViewById(R.id.tap_me_button)
-        tapMeButton.setOnClickListener { incrementScore() }
+        tapMeButton.setOnClickListener { view ->
+            val bounceAnimation = AnimationUtils.loadAnimation(this, R.anim.bounce)
+            view.startAnimation(bounceAnimation)
+            incrementScore() }
         if (savedInstanceState != null) {
             score = savedInstanceState.getInt(SCORE_KEY)
             timeLeft = savedInstanceState.getInt(TIME_LEFT_KEY)
